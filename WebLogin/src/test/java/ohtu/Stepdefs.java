be3209b202ef8d354cf6ca89d00fd3622ecc16b3
Pasi Alho <alho.pasi@iki.fi>
2019-11-13 15:27:44 +0200

    WebLogin added

---
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -0,0 +1,78 @@
+package ohtu;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class Stepdefs {
+    WebDriver driver = new FirefoxDriver();
+    String baseUrl = "http://localhost:4567";
+
+    @Given("login is selected")
+    public void loginIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+    }
+
+    @When("correct username {string} and password {string} are given")
+    public void correctUsernameAndPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }
+
+    @Then("user is logged in")
+    public void userIsLoggedIn() {
+        pageHasContent("Ohtu Application main page");
+    }
+
+    @When("correct username {string} and incorrect password {string} are given")
+    public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }
+
+    @Then("user is not logged in and error message is given")
+    public void userIsNotLoggedInAndErrorMessageIsGiven() {
+        pageHasContent("invalid username or password");
+        pageHasContent("Give your credentials to login");
+    }
+
+    @When("incorrect username {string} and password {string} are given")
+    public void incorrectUsernameAndPasswordAreGiven(String username, String password) throws InterruptedException {
+        logInWith(username, password);
+        System.out.println(username + "  " + password);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    /* helper methods */
+
+    private void pageHasContent(String content) {
+        sleep(1);
+        assertTrue(driver.getPageSource().contains(content));
+    }
+
+    private void logInWith(String username, String password) {
+        sleep(1);
+        assertTrue(driver.getPageSource().contains("Give your credentials to login"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("login"));
+        element.submit();
+    }
+
+    private void sleep(int seconds) {
+        try { Thread.sleep(seconds*1000); } catch(Exception e) {};
+    }
+}
